c5c44ba501ce6c1ccd7d858dc1e3d71e97951c68
Oughlane <oughlanemohammed72@gmail.com>
2025-07-18 16:47:22 +0200

    add buuton input and structure for loginpop out

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {}
+  const modal = document.querySelector(".modal");
+  const closeButton = document.querySelector(".close-button");
+  const openModalButtons = document.querySelectorAll(".open-modal");
+}
